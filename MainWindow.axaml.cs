4ff74446922161b855544627dfa46a21d7071118
anastasia <baluevanasty@gmail.com>
2025-02-18 12:35:55 +0300

    third commit

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -5,13 +5,16 @@ using Northwoods.Go;
 using Northwoods.Go.Layouts;
 using Northwoods.Go.Models;
 using Northwoods.Go.PanelLayouts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using static mat_modelirovanije.Helper;
+using Panel = Northwoods.Go.Panel;
 using TextBlock = Northwoods.Go.TextBlock;
 
-public class MyModel : TreeModel<NodeData, string, object> { }
-public class NodeData : MyModel.NodeData 
+public class MyModel : TreeModel<NodeData, string, object> {
+}
+public class NodeData : MyModel.NodeData
 {
     public string Name { get; set; }
 }
@@ -21,12 +24,16 @@ namespace mat_modelirovanije
     public partial class MainWindow : Window
     {
         private List<Department> _Department = DBContext.Departments.Include(x => x.MainDeps).Include(x => x.LowerDeps).Include(x => x.Employees).Include(x => x.DepartmentDirectorNavigation).ToList();
+        private List<Employee> _Employees = DBContext.Employees.Include(x => x.Job).Include(x => x.Department).ToList();
         private Diagram _myDiagram;
         public MainWindow()
         {
             InitializeComponent();
 
+            lbox_employee.ItemsSource = _Employees.ToList();
             _myDiagram = diagramControl.Diagram;
+
+            
 
             Setup();
         }
@@ -34,16 +41,29 @@ namespace mat_modelirovanije
         private void Setup()
         {
             _myDiagram.UndoManager.IsEnabled = false;
-            _myDiagram.Layout = new TreeLayout { Angle = 90, LayerSpacing = 35 };
+            _myDiagram.Layout = new TreeLayout { Angle = 90, LayerSpacing = 50 };
+
+            var DisplayEmployee = new Action<InputEvent, GraphObject>((e, obj) => {
+                while (true) ;
+            });
 
             _myDiagram.NodeTemplate = new Node("Vertical")
             {
-                Background = "#e1f4c8"
+                
+            }.Add(new Panel()
+            {
+                Background = "#e1f4c8",
+                Width = 120,
+                DoubleClick = DisplayEmployee
+
             }.Add(new TextBlock("Default Text")
             {
+                Wrap = Wrap.DesiredSize,
                 Margin = 12,
                 Stroke = "black"
-            }.Bind("Text", "Name"));
+            }.Bind("Text", "Name")));
+
+            
 
             List<NodeData> nodes = new List<NodeData>();
 
@@ -57,5 +77,34 @@ namespace mat_modelirovanije
                 NodeDataSource = nodes
             };
         }
+
+        //private void Setup()
+        //{
+        //    _myDiagram.UndoManager.IsEnabled = false;
+        //    _myDiagram.Layout = new TreeLayout { Angle = 90, LayerSpacing = 50 };
+
+        //    _myDiagram.NodeTemplate = new Node("Vertical")
+        //    {
+        //        Background = "#e1f4c8",
+        //        Width = 120
+        //    }.Add(new TextBlock("Default Text")
+        //    {
+        //        Wrap = Wrap.DesiredSize,
+        //        Margin = 12,
+        //        Stroke = "black"
+        //    }.Bind("Text", "Name"));
+
+        //    List<NodeData> nodes = new List<NodeData>();
+
+        //    foreach (Department department in _Department)
+        //    {
+        //        nodes.Add(new NodeData() { Name = department.Name, Key = $"{department.Id}", Parent = $"{department.IdMainpepartment}" });
+        //    }
+
+        //    _myDiagram.Model = new MyModel
+        //    {
+        //        NodeDataSource = nodes
+        //    };
+        //}
     }
 }
